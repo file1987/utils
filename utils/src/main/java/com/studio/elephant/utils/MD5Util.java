ae21a6cde5cd95558115860fdce591ddba765cdd
file1987 <laowen87@163.com>
2015-01-21 10:06:26 +0800

    Md5修改方法

--- utils/src/main/java/com/studio/elephant/utils/MD5Util.java
+++ utils/src/main/java/com/studio/elephant/utils/MD5Util.java
@@ -38,5 +38,37 @@ public class MD5Util {
         }    
         return md5StrBuff.toString();  
 	}
+	
+	public static String digest(String txt) {
+		MessageDigest md;
+		try {
+
+			md = MessageDigest.getInstance("MD5");
+			md.update(txt.getBytes("UTF-8"));
+
+			return toHex(md.digest());
+
+		} catch (NoSuchAlgorithmException e) {
+			return "";
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+	private static final char[] hexDigits = { '0', '1', '2', '3', '4', '5',
+			'6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+	private static String toHex(byte[] byteArray) {
+
+		char[] resultCharArray = new char[byteArray.length * 2];
+		int index = 0;
+		for (byte b : byteArray) {
+			resultCharArray[index++] = hexDigits[b >>> 4 & 0xf];
+			resultCharArray[index++] = hexDigits[b & 0xf];
+		}
+		return new String(resultCharArray);
+	}
+
 
 }
